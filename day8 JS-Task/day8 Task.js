abf00df627507c10b00cc3566472084ef37efbb3
GouthamSomaraju <mr.goutham00@gmail.com>
2024-10-24 14:34:56 +0530

    day8 task

---
+++ day8 JS-Task/day8 Task.js
@@ -0,0 +1,107 @@
+// task:
+// ------
+// 1.Traffic Light System
+// Simulate a traffic light system:
+
+// If the light is red, log "Stop."
+// If the light is yellow, log "Get ready to move."
+// If the light is green, log "Go."
+// If the input doesn't match any color, log "Invalid input."
+
+var light="red";
+
+if(light=="red"){
+
+   console.log("Stop.")
+
+}else if(light=="yellow"){
+
+   console.log("Get ready to move.")
+
+}else if(light=="green"){
+  
+   console.log("Go.")
+
+}else{
+
+   console.log("Invalid input")
+
+}
+
+
+// 2.Weather Description (Temperature Descriptions)
+// Write a program that describes the temperature:
+
+// If the temperature is exactly 0, log "It's freezing cold."
+// If the temperature is exactly 15, log "It's a cool day."
+// If the temperature is exactly 25, log "It's a pleasant day."
+// If the temperature doesn't match any of these, log "Unknown weather."
+
+var temperature=0;
+
+if(temperature==0){
+
+   console.log("freezing cold")
+
+}else if(temperature==15){
+
+   console.log("It's a cool day")
+
+}else if(temperature==25){
+
+   console.log("It's a pleasant day")
+
+}else{
+
+   console.log("Unknown weather")
+
+};
+
+// 3.: Day of the Week
+
+// Create a program that takes input of a number representing the day of the week (1-7):
+
+// If the number is 1, log "Today is Monday."
+// If the number is 2, log "Today is Tuesday."
+// If the number is 3, log "Today is Wednesday."
+// If the number is 4, log "Today is Thursday."
+// If the number is 5, log "Today is Friday."
+// If the number is 6, log "Today is Saturday."
+// If the number is 7, log "Today is Sunday."
+// If the input is not between 1 and 7, log "Invalid day number."
+
+var week_day=7;
+
+if(week_day==1){
+
+   console.log("Today is Monday")
+
+}else if(week_day==2){
+
+   console.log("Today is Tuesday")
+
+}else if(week_day==3){
+
+   console.log("Today is Wednesday")
+
+}else if(week_day==4){
+
+   console.log("Today is Thrusday")
+
+}else if(week_day==5){
+
+   console.log("Today is Friday")
+
+}else if(week_day==6){
+
+   console.log("Today is Saturday")
+
+}else if(week_day==7){
+
+   console.log("Today is Sunday")
+
+}else{
+
+   console.log("Invalid day Number")
+
+}
