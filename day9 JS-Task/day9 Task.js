c6d307096faf0a2542695af7ccf5377b3419ad26
GouthamSomaraju <mr.goutham00@gmail.com>
2024-10-25 21:23:47 +0530

    day9 task using prompt & alert

---
+++ day9 JS-Task/day9 Task.js
@@ -0,0 +1,42 @@
+// 1
+// var number=prompt("enter the number")
+//         if(number%2==0){
+//             console.log(alert("Even"))
+//         }else{
+//             console.log(alert("Odd"))
+//         }
+
+// // 2
+// var num1=prompt("enter 1st value")
+
+// var num2=prompt("enter 2st value")
+
+// if(num1>num2){
+//     console.log(alert(`${num1} is "Greater" than ${num2}`))
+// }else if(num1==num2){
+//     console.log(alert(`${num1} and ${num2} are equal numbers`))
+// }else{
+//     console.log(alert(`${num1} is "Smaller" than ${num2}`))
+// }
+
+// 3
+// var age=prompt("enter your age")
+// var license= prompt("Do you have valid drivers license(true/false)")
+
+// if(age>=18 & license==='true'){
+//     console.log(alert("Can Drive"))
+// }
+// else{
+//     console.log(alert("Cannot Drive"))
+// }
+
+// 4
+// var num = prompt("enter number:");
+
+// if (num > 0) {
+//   console.log(alert("Positive Number"));
+// } else if (num < 0) {
+//   console.log(alert("Negative Number"));
+// } else {
+//   console.log(alert("Zero"));
+// }
