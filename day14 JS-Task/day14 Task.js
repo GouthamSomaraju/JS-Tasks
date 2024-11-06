ec99f29069b9e9f51a84567924ddc89a35e2c85f
GouthamSomaraju <mr.goutham00@gmail.com>
2024-11-06 22:57:48 +0530

    Task using by function definition

---
+++ day14 JS-Task/day14 Task.js
@@ -0,0 +1,48 @@
+
+// 1.write a function to check whether input num is even or odd 
+function number(x){
+    if(x%2==0){
+        console.log(`${x} is even number`)
+    }
+    else{
+        console.log(`${x} is odd number`)
+    }
+}
+number(6)
+
+// ------------------------------------------------------------------------------------------------------------
+
+
+
+// 2.write a function to check whether input string is palindrome or not
+var b=""
+function str(a){
+ for(i=a.length-1;i>=0;i--){
+    b+=a[i]
+ }
+//  console.log(b)
+ if(b===a){
+    console.log("Palindrome")
+ }else{
+    console.log("not Palindrome")
+ }
+}
+str("mom")
+
+// -------------------------------------------------------------------------------------------------------------------
+
+
+// 3.write a function to check largest num among three numbers.
+
+function nums(a,b,c){
+    if(a>b && a>c){
+        console.log(`a = ${a} is greater number than b, c `)
+    }
+    if(b>c && b>a){
+        console.log(`b = ${b} is greater number c, a`)
+    }
+    if(c>a && c>b){
+        console.log(`c = ${c} is greater number a, b`)
+    }
+}
+nums(1,3,9);
